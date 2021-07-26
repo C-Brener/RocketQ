54cf08fb47dcd745842e384b022cec4465842e98
brenermr <brenercaique0806@gmail.com>
2021-07-25 22:11:57 -0300

    Rocket.Q

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,6 +2,12 @@ import Modal from './modal.js'
 
 const modal = Modal()
 
+const modalTitle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
+
+
+
 //Pegar todos os botões que existe com a classe check
 
 const checkButtons = document.querySelectorAll(".actions a.check")
@@ -9,10 +15,7 @@ const checkButtons = document.querySelectorAll(".actions a.check")
 checkButtons.forEach(button =>{
     // add a escuta para identificar se o botão foi clicado
 
-    button.addEventListener("click", event => {
-        // Abrindo a modal 
-        modal.open()
-    })
+    button.addEventListener("click", handleClick)
 });
 
 
@@ -20,8 +23,22 @@ checkButtons.forEach(button =>{
 const deleteButton = document.querySelectorAll(".actions a.delete")
 
 deleteButton.forEach(button =>{
-    button.addEventListener("click", event =>{
-        modal.open()
-    })
+    button.addEventListener("click", (event) => handleClick(event, false)) //Caso a variavel check seja alterada de true para false executara o codigo padraõa do html
 
+    
 });
+
+function handleClick(event, check = true){
+    event.preventDefault()
+
+
+    // a interrogação server para verificar qual o valor booleano que a variavel retornará sabendo que o balor padrão é true
+    modalTitle.innerHTML = check ? "Marcar como Lido" : "Excluir essa pergunta"
+    modalDescription.innerHTML = check ? "Deseja mesmo marcar essa questão como lida?" : "Deseja excluir essa pergunta?"
+    modalButton.innerHTML = check ? "Sim, marcar como lida." : "Sim, excluir."
+    check? modalButton.classList.remove("red") : modalButton.classList.add("red")
+
+    
+    // abrindo a modal 
+    modal.open()
+}
