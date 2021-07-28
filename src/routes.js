ccdabfaeb1906bb1737043c7b8716ba77a757e69
brenermr <brenercaique0806@gmail.com>
2021-07-28 00:00:03 -0300

    Rocket.Q

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,22 @@
 
 const express = require('express')
 const QuestionController = require('./controllers/QuestionController')
+const RoomController = require('./controllers/RoomController')
 
 const route = express.Router()
 
-route.get('/', (req, res) => res.render("index"))
+route.get('/', (req, res) => res.render("index", {page:'enter-room'}))
 
-route.get('/room', (req, res) => res.render("room"))
+route.get('/create-pass', (req, res) => res.render("index", {page:'create-pass'}))
 
-route.get('/create-pass', (req, res) => res.render("create-pass"))
+route.get('/room/:room', (req, res) => res.render("room"))
+
 
 //Formato que o formulario da modal tem que passar informação
 route.post('/room/:room/:question/:action/', QuestionController.index)
+
+route.post('/room/create-room', RoomController.create)
+
 
 //O req sempre retornara um objeto, dito isso o mesmo precisa estar dentro de chaves
 // O sinal de : seguido de um nome define que a rota sera variavel de acordo com seu uso.
