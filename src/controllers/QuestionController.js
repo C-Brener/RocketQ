44cd07b31cfbe55618b07cac8777950d1e5525e7
brenermr <brenercaique0806@gmail.com>
2021-08-03 18:44:31 -0300

    Rocket.Q

--- src/controllers/QuestionController.js
+++ src/controllers/QuestionController.js
@@ -2,13 +2,25 @@ const Database = require('../db/config')
 
 module.exports = { // criando o modulo pra ser exportado
     
-    index(req,res){
+    async index(req,res){
+        const db = await Database()
         const roomId = req.params.room
         const questionId = req.params.question
         const action = req.params.action
-        const password = req.body.password
+        const password = req.body.password 
 
-        console.log(`room = ${roomId}, questionId = ${questionId}, action = ${action}, password = ${password}`)
+        /*Verificando se a senha está correta*/
+        const verifyRoom = await db.get(`SELECT * FROM rooms WHERE id = ${roomId}`)
+        if(verifyRoom.pass == password){
+            if(action == "delete"){
+                await db.run(`DELETE FROM questions WHERE id = ${questionId}`)
+
+            }else if (action == "check"){
+                await db.run(`UPDATE questions SET read = 1 WHERE id = ${questionId}`)
+            }
+        }
+        res.redirect(`/room/${roomId}`)
+
     },
     async create(req,res){
        const db = await Database()
