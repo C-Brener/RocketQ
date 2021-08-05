f9014deddb445455f4c9837add0603b5d7bdacca
brenermr <brenercaique0806@gmail.com>
2021-08-04 21:05:09 -0300

    Rocket.Q

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -45,8 +45,14 @@ module.exports ={
         const roomId = req.params.room
         const questions = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 0`)
         const questionsRead = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 1`)
+        let isNoQuestions = false
 
-        res.render("room", {roomId: roomId, questions: questions, questionsRead: questionsRead})
+        if(questions.length == 0){
+            if(questionsRead.length == 0){
+                isNoQuestions = true
+            }
+        }
+        res.render("room", {roomId: roomId, questions: questions, questionsRead: questionsRead, isNoQuestions: isNoQuestions})
 
     },
     enter(req, res){
